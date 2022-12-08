74ae08614b064e237ec7128c6e2efee232067c2e
Fernando Lopez <flopez00032@gmail.com>
2022-12-08 15:10:48 -0600

    deleted study verse directory

--- src/main/java/com/studyverse/StudyVerseApplication.java
+++ src/main/java/com/studyverse/StudyVerseApplication.java
@@ -1,4 +1,4 @@
-package com.studyverse.studyverse;
+package com.studyverse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
