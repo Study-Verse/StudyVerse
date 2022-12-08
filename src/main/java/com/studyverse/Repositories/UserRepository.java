7abbcb5a302abda015418d4feb909b407c634100
David Lara <david.a.lara05@gmail.com>
2022-12-08 16:39:41 -0600

    created repo and controller

--- src/main/java/com/studyverse/Repositories/UserRepository.java
+++ src/main/java/com/studyverse/Repositories/UserRepository.java
@@ -1,4 +1,8 @@
-package com.studyverse.Repositories;
-
-public interface UserRepository {
-}
+//package com.studyverse.Repositories;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface UserRepository extends JpaRepository<User, Long> {
+//    User findById(long id);
+//    User findByUsername(String username);
+//}
