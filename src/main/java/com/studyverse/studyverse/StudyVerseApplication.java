08c28297c3822865a424fdc6b7c9550159ee9787
Fernando Lopez <flopez00032@gmail.com>
2022-12-08 15:00:23 -0600

    initial commit

---
+++ src/main/java/com/studyverse/studyverse/StudyVerseApplication.java
@@ -0,0 +1,13 @@
+package com.studyverse.studyverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyVerseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyVerseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/studyverse/studyverse/StudyVerseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studyverse.studyverse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyVerseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
