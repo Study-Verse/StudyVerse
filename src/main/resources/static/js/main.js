a2d61ac0264f0eaabd7c35bc9069edc6bf79e8d9
Nazareth Briseno <nazarethbriseno@gmail.com>
2022-12-12 09:05:37 -0600

    Login Add ons

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,5 +1,22 @@
-$("#register").onclick(function(){
-    $("#login").toggleClass("display-none");
+let signup = document.querySelector(".signup");
+let login = document.querySelector(".login");
+let slider = document.querySelector(".slider");
+let formSection = document.querySelector(".form-section");
+
+
+
+
+$(".signup").click(function(){
+    $(".slider").classList.add("moveslider");
+    $(".form-section").classList.add("form-section-move");
 })
 
-alert("Hello");
+// signup.addEventListener("click", function() {
+//     slider.
+//     formSection
+// });
+
+login.click(function() {
+    slider.classList.remove("moveslider");
+    formSection.classList.remove("form-section-move");
+});
