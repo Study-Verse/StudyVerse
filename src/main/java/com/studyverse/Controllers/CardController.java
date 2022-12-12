a611baad6e41f2b176b1a5e58414ca7ccf3cfc69
David Lara <david.a.lara05@gmail.com>
2022-12-12 10:07:15 -0600

    saved so i can merge main

--- src/main/java/com/studyverse/Controllers/CardController.java
+++ src/main/java/com/studyverse/Controllers/CardController.java
@@ -1,19 +1,54 @@
 package com.studyverse.Controllers;
 
+import com.studyverse.Models.Card;
+import com.studyverse.Models.User;
+import com.studyverse.Repositories.CardRepository;
+import com.studyverse.Repositories.UserRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @Controller
+@RequestMapping("/flash")
 public class CardController {
+
+    private final CardRepository cardDao;
+
+    private final UserRepository userDao;
+
+    public CardController(CardRepository cardDao, UserRepository userDao) {
+        this.cardDao = cardDao;
+        this.userDao = userDao;
+    }
 
     @GetMapping()
     public String landingPage(){
         return "splashpage";
     }
 
+
+//    This lets you create a card set
+    @GetMapping("/create")
+    public String createCard(Model model){
+        List<User>users = userDao.findAll();
+        model.addAttribute("users", users);
+        model.addAttribute("card", new Card());
+        return "createCard";
+    }
+
+
+
+//    This let you post your card set
+//    @PostMapping("/create")
+//    public String postCard(@ModelAttribute Card card){
+//
+//    }
+//
 
 
 }// END OF CARD CONTROLLER
--- src/main/java/com/studyverse/Repositories/CardRepository.java
+++ src/main/java/com/studyverse/Repositories/CardRepository.java
@@ -1,8 +1,9 @@
 package com.studyverse.Repositories;
 
 import com.studyverse.Models.Card;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CardRepository {
+public interface CardRepository extends JpaRepository<Card, Long> {
     Card findById(long id);
 
 }
