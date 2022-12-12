e4f28702187ed793f260682218d9e4ef8c44e368
David Lara <david.a.lara05@gmail.com>
2022-12-11 18:17:23 -0600

    created getmappings

--- src/main/java/com/studyverse/Controllers/CardController.java
+++ src/main/java/com/studyverse/Controllers/CardController.java
@@ -1,10 +1,12 @@
 package com.studyverse.Controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
+@Controller
 public class CardController {
 
     @GetMapping()
