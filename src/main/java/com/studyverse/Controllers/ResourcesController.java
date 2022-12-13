a539c9d1d654b35ee8dbaa51e898566c10651e05
David Lara <david.a.lara05@gmail.com>
2022-12-13 11:44:20 -0600

    working on booksview

--- src/main/java/com/studyverse/Controllers/ResourcesController.java
+++ src/main/java/com/studyverse/Controllers/ResourcesController.java
@@ -1,7 +1,9 @@
 package com.studyverse.Controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class ResourcesController {
@@ -9,6 +11,13 @@ public class ResourcesController {
     @GetMapping("/books")
     public String showBookPage(){
         return "/resources";
+    }
+
+
+    @GetMapping("/bookView/{isbn}")
+    public String showBookView(Model model, @PathVariable int isbn){
+        model.addAttribute("isbn", isbn);
+        return "/bookView";
     }
 
 }
---
+++ src/main/resources/static/js/books.js
@@ -0,0 +1,117 @@
+$(document).ready(function() {
+    // let item, title, author, publisher, bookLink, bookImg;
+    let outputList = document.getElementById("list-output");
+    let bookUrl = "https://www.googleapis.com/books/v1/volumes?q=";
+    let placeHldr = '<img src="https://via.placeholder.com/150">';
+    let searchData;
+
+    //handling error for empty search box
+    function displayError() {
+        alert("search term can not be empty!")
+    }
+
+    //listener for search button
+    $("#search").click(function() {
+        outputList.innerHTML = ""; //empty html output
+        document.body.style.backgroundImage = "url()";
+        searchData = $("#search-box").val();
+        //handling empty search input field
+        if(searchData === "" || searchData === null) {
+            displayError();
+        }
+        else {
+            // console.log(searchData);
+            // $.get("https://www.googleapis.com/books/v1/volumes?q="+searchData, getBookData()});
+            $.ajax({
+                url: bookUrl + searchData,
+                dataType: "json",
+                success: function(response) {
+                    console.log(response)
+                    if (response.totalItems === 0) {
+                        alert("no result!.. try again")
+                    }
+                    else {
+                        $("#title").animate({'margin-top': '5px'}, 1000); //search box animation
+                        $(".book-list").css("visibility", "visible");
+                        displayResults(response);
+                    }
+                },
+                error: function () {
+                    alert("Something went wrong.. <br>"+"Try again!");
+                }
+            });
+        }
+        $("#search-box").val(""); //clean search box
+    });
+
+    /*
+    * function to display result in index.html
+    * @param response
+    */
+
+    // This displays the results and we are doing += 2 since we are displaying 2 books
+    function displayResults(response) {
+        for (let i = 0; i < response.items.length; i+=2) {
+            item = response.items[i];
+            title1 = item.volumeInfo.title;
+            author1 = item.volumeInfo.authors;
+            publisher1 = item.volumeInfo.publisher;
+            bookLink1 = item.volumeInfo.previewLink;
+            console.log("changes on line 60")
+            bookIsbn = item.volumeInfo.industryIdentifiers[1].identifier
+            bookImg1 = (item.volumeInfo.imageLinks) ? item.volumeInfo.imageLinks.thumbnail : placeHldr ;
+
+            item2 = response.items[i+1];
+            title2 = item2.volumeInfo.title;
+            author2 = item2.volumeInfo.authors;
+            publisher2 = item2.volumeInfo.publisher;
+            bookLink2 = item2.volumeInfo.previewLink;
+            bookIsbn2 = item2.volumeInfo.industryIdentifiers[1].identifier
+            bookImg2 = (item2.volumeInfo.imageLinks) ? item2.volumeInfo.imageLinks.thumbnail : placeHldr ;
+
+            // in production code, item.text should have the HTML entities escaped.
+            outputList.innerHTML += '<div class="row mt-4">' +
+                formatOutput(bookImg1, title1, author1, publisher1, bookLink1, bookIsbn) +
+                formatOutput(bookImg2, title2, author2, publisher2, bookLink2, bookIsbn2) +
+                '</div>';
+
+            console.log(outputList);
+        }
+    }
+
+    /*
+    * card element formatter using es6 backticks and templates (individual card)
+    * @param bookImg title author publisher bookLink
+    * @return htmlCard
+    */
+    // This is creating the cards
+    function formatOutput(bookImg, title, author, publisher, bookLink, bookIsbn) {
+        // console.log(title + ""+ author +" "+ publisher +" "+ bookLink+" "+ bookImg)
+
+        // (BAD) http://localhost:63342/Practice-Code/bookView.html?isbn=9780131872486
+        // (GOOD) http://localhost:63342/Practice-Code/bookView.html?_ijt=9780991229048=RELOAD_ON_SAVE
+        let viewUrl = 'bookView.html?isbn='+bookIsbn; //constructing link for book viewer
+        //This creates the cards
+        let htmlCard = `<div class="col-lg-6">
+       <div class="card" style="">
+         <div class="row no-gutters">
+           <div class="col-md-4">
+             <img src="${bookImg}" class="card-img" alt="...">
+           </div>
+           <div class="col-md-8">
+             <div class="card-body">
+               <h5 class="card-title">${title}</h5>
+               <p class="card-text">Author: ${author}</p>
+               <p class="card-text">Publisher: ${publisher}</p>
+               <a target="_blank" href="${viewUrl}" class="btn btn-secondary">Read Book</a>
+             </div>
+           </div>
+         </div>
+       </div>
+     </div>`
+        return htmlCard;
+    }
+
+
+
+});
