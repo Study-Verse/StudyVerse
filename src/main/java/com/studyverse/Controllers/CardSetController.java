add2a81057c9d7bd8d59c0fc891a078422cd944f
Nazareth Briseno <nazarethbriseno@gmail.com>
2022-12-13 15:03:39 -0600

    Users can now view their own list of cardsets on their dashboard

--- src/main/java/com/studyverse/Controllers/CardSetController.java
+++ src/main/java/com/studyverse/Controllers/CardSetController.java
@@ -3,6 +3,7 @@ package com.studyverse.Controllers;
 import com.studyverse.Models.CardSet;
 import com.studyverse.Models.User;
 import com.studyverse.Repositories.CardSetRepository;
+import com.studyverse.Repositories.UserRepository;
 import com.studyverse.Services.Utils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,13 +11,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class CardSetController {
 
     private final CardSetRepository cardSetDao;
 
-    public CardSetController(CardSetRepository cardSetDao) {
+    private final UserRepository userDao;
+
+    public CardSetController(CardSetRepository cardSetDao, UserRepository userDao) {
         this.cardSetDao = cardSetDao;
+        this.userDao = userDao;
     }
 
 
@@ -32,6 +38,15 @@ public class CardSetController {
         cardSet.setUser(user);
         cardSetDao.save(cardSet);
         return "redirect:/card-create";
+    }
+
+    //    ============ dashboard get mapping
+    @GetMapping("/dashboard")
+    public String dashboard(Model model){
+        long user = Utils.currentUser().getId();
+        List<CardSet> cardSetList = cardSetDao.findByUserId(user);
+        model.addAttribute("cardSetList",cardSetList);
+        return "/dashboard";
     }
 
 
