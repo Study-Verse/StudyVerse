dd1bd4ed226e7e184ff7d740a0e9252394c0d886
Fernando Lopez <flopez00032@gmail.com>
2022-12-14 14:47:11 -0600

    study view

--- src/main/resources/static/js/study.js
+++ src/main/resources/static/js/study.js
@@ -1,12 +1,10 @@
 $(function (){
 
-    let cardList =
         function getCardList(){
                 fetch("card-api")
                 .then(response => response.json())
                 .then(data =>{
                     console.log(data)
-
                     data.forEach((card) =>{
                         console.log(card)
                         $("#study-card-container").prepend(`
@@ -15,20 +13,17 @@ $(function (){
                         `)
                     })// end of for each
                 })
-
-
 }
+
 
     //get the first card in the array of objects from json
     function getFirstCard(){
-        return cardList()
     }
 
     //get the current card index in the list and add one to the index to get the next one
     function getNextCard(cardListArray){
+    }
 
-
-}
 
 
 
