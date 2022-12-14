b9e0680247156b386315ca120e7cdf189d3cb3fc
Fernando Lopez <flopez00032@gmail.com>
2022-12-14 14:00:45 -0600

    changed for of to forEach loop on card list

--- src/main/resources/static/js/study.js
+++ src/main/resources/static/js/study.js
@@ -6,15 +6,14 @@ function getCardList(){
         .then(response => response.json())
         .then(data =>{
             console.log(data)
-            // data.forEach((card) =>{
-            for (let card of data) {
+            data.forEach((card) =>{
                 console.log(card)
                 $("#study-card-container").prepend(`
                     <p>${card.frontFace}</p>
                     <p>${card.backFace}</p>
                 `)
-                // })
-            }
+                })
+
         })
 }
 getCardList();
