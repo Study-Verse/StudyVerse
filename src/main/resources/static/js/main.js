b0fd55ebfffb2883586bf2842cdf8e1c71aa04f3
Nazareth Briseno <nazarethbriseno@gmail.com>
2022-12-14 11:27:23 -0600

    Added individual modals for card sets to edit

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,7 +1,31 @@
-$(".card-sets").mouseenter(function (){
-    $(".svg").removeClass("display-none");
-})
 
-$(".card-sets").mouseleave(function (){
-    $(".svg").addClass("display-none");
-})
+//function for letting the SVGs show on mouseOver
+if(window.innerWidth > 768){
+    $(".card-sets").mouseenter(function (){
+        $(this).children(".svg").removeClass("display-none");
+    })
+
+    $(".card-sets").mouseleave(function (){
+        $(this).children(".svg").addClass("display-none");
+    })
+}
+
+//Function to show the SVGs automatically if the screen size is smaller than 768, this doesn't work on resizing the
+// screen only if you start off at that screen size
+if(window.innerWidth < 768){
+    $(".svg").removeClass("display-none");
+}
+
+
+//Modal Functions
+$(document).ready(function(){
+
+    $(".edit-svg").on("click", function(){
+        $(this).parent().siblings(".card-modal").removeClass("display-none");
+    })//End of modal on click function
+    $(".close").on("click", function(){
+        $(".card-modal").addClass("display-none");
+    })
+})//End of document.ready
+
+//End of Modal Functions
