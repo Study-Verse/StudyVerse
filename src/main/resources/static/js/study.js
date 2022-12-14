8eb48a8eac6e000fcd92a5932b4e9cb16eb3e9a5
Fernando Lopez <flopez00032@gmail.com>
2022-12-14 13:44:34 -0600

    funcitonality for populating card from set

--- src/main/resources/static/js/study.js
+++ src/main/resources/static/js/study.js
@@ -1,10 +1,21 @@
 $(function (){
 
 function getCardList(){
+
     fetch("card-api")
         .then(response => response.json())
-        .then(data =>
-        console.log(data))
+        .then(data =>{
+            console.log(data)
+            // data.forEach((card) =>{
+            for (let card of data) {
+                console.log(card)
+                $("#study-card-container").prepend(`
+                    <p>${card.frontFace}</p>
+                    <p>${card.backFace}</p>
+                `)
+                // })
+            }
+        })
 }
 getCardList();
 
