2508463ced2623beefa86fad926e1f0bc399b533
Fernando Lopez <flopez00032@gmail.com>
2022-12-14 14:33:37 -0600

    added function skeleton to manipulate what card shows on the screen

--- src/main/resources/static/js/study.js
+++ src/main/resources/static/js/study.js
@@ -1,28 +1,34 @@
 $(function (){
 
-function getCardList(){
+    let cardList =
+        function getCardList(){
+                fetch("card-api")
+                .then(response => response.json())
+                .then(data =>{
+                    console.log(data)
 
-    let arrayOfCards =[]
-    arrayOfCards =
-
-        fetch("card-api")
-        .then(response => response.json())
-        .then(data =>{
-            console.log(data)
-
-            data.forEach((card) =>{
-                console.log(card)
-                $("#study-card-container").prepend(`
-                    <p>${card.frontFace}</p>
-                    <p>${card.backFace}</p>
-                `)
-            })// end of for each
-        })
+                    data.forEach((card) =>{
+                        console.log(card)
+                        $("#study-card-container").prepend(`
+                            <p>${card.frontFace}</p>
+                            <p>${card.backFace}</p>
+                        `)
+                    })// end of for each
+                })
 
 
 }
-getCardList();
 
+    //get the first card in the array of objects from json
+    function getFirstCard(){
+        return cardList()
+    }
+
+    //get the current card index in the list and add one to the index to get the next one
+    function getNextCard(cardListArray){
+
+
+}
 
 
 
