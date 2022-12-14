c1aca8aa1ba0daad7c200bb61c64142c34cc90cc
Fernando Lopez <flopez00032@gmail.com>
2022-12-14 14:18:04 -0600

    cleaned up code and will start on functions to show one card at a time

--- src/main/resources/static/js/study.js
+++ src/main/resources/static/js/study.js
@@ -2,19 +2,24 @@ $(function (){
 
 function getCardList(){
 
-    fetch("card-api")
+    let arrayOfCards =[]
+    arrayOfCards =
+
+        fetch("card-api")
         .then(response => response.json())
         .then(data =>{
             console.log(data)
+
             data.forEach((card) =>{
                 console.log(card)
                 $("#study-card-container").prepend(`
                     <p>${card.frontFace}</p>
                     <p>${card.backFace}</p>
                 `)
-                })
-
+            })// end of for each
         })
+
+
 }
 getCardList();
 
