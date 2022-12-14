b865c24141c3290cd30f939a98f4ff84a4910ea5
Fernando Lopez <flopez00032@gmail.com>
2022-12-13 22:02:26 -0600

    targeted the id of the card set using jquery for edit functionality

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,11 +1,25 @@
-$(".signup").click(function(){
-    $(".slider").addClass("moveslider");
-    $(".form-section").addClass("form-section-move");
+$(function (){
+
+    $(".signup").click(function(){
+        $(".slider").addClass("moveslider");
+        $(".form-section").addClass("form-section-move");
+    });
+
+    $(".login").click(function() {
+        $(".slider").removeClass("moveslider");
+        $(".form-section").removeClass("form-section-move");
+    });
+
+// alert("yo");
+//
+//     $(".edit-button").on("click",function (){
+//         console.log($(this).attr("data-id"));
+//         window.location.replace(`/post/${$(this).attr("data-id")}/edit`);
+//     });
+    $(".edit-card-set").on("click",function (){
+        // alert("yo");
+        console.log($(this).attr("set-id"));
+    })
+
 });
 
-$(".login").click(function() {
-    $(".slider").removeClass("moveslider");
-    $(".form-section").removeClass("form-section-move");
-
-  alert("yoooo");
-});
