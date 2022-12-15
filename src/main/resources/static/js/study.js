63d536d84c8ec1638e903433f98c0c6ba7207bf0
Fernando Lopez <flopez00032@gmail.com>
2022-12-15 10:24:14 -0600

    added a css file for study

--- src/main/resources/static/js/study.js
+++ src/main/resources/static/js/study.js
@@ -1,24 +1,41 @@
 $(function (){
 
-        function getCardList(){
+    function getCardList(){
+        getFirstCard()
                 fetch("card-api")
                 .then(response => response.json())
                 .then(data =>{
-                    console.log(data)
-                    data.forEach((card) =>{
-                        console.log(card)
-                        $("#study-card-container").prepend(`
-                            <p>${card.frontFace}</p>
-                            <p>${card.backFace}</p>
-                        `)
-                    })// end of for each
+                    getFirstCard(data)
+                    
+                    // .then($("#study-card-container").append(`
+                    //
+                    //       <div class="carousel-inner">
+                    //             <div class="carousel-item active">
+                    //                   <p>${data.frontFace}</p>
+                    //                   <p>${data.backFace}</p>
+                    //             </div>
+                    //       </div>
+                    //     `)
+                    // )
+                    // data.forEach((card) =>{
+                    //     console.log(card)
+                    //     $("#study-card-container").append(`
+                    //       <div class="carousel-inner">
+                    //         <div class="carousel-item active">
+                    //           <p>${card.frontFace}</p>
+                    //           <p>${card.backFace}</p>
+                    //         </div>
+                    //       </div>
+                    //     `)
+                    // })// end of for each
                 })
-}
-getCardList()
+}// end of getCardList function
+    getCardList()
 
 
     //get the first card in the array of objects from json
-    function getFirstCard(){
+    function getFirstCard(array){
+        return array[0]
     }
 
     //get the current card index in the list and add one to the index to get the next one
