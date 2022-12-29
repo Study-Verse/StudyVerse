705afaa418d6f9ad7989c8392945ed5d000924c8
Fernando Lopez <flopez00032@gmail.com>
2022-12-29 11:31:23 -0600

    added style to cards, need to add flip functionality

--- src/main/resources/static/js/study.js
+++ src/main/resources/static/js/study.js
@@ -1,22 +1,23 @@
 $(function (){
     // getting the ID from the window #no need for hidden div
-    console.log(window.location.pathname)
     let cardSetId = window.location.pathname.split("/")[2];
     function getCardList(){
                 fetch(`/card-api/${cardSetId}`)
                 .then(response => response.json())
                 .then(data =>{
-                    console.log(data)
                     data.forEach((card) =>{
-                        $("#study-card-container").append(`
+                        $(".study-card-container").append(`
                         <div class="flashcard">
-                               <div class="card-body"
-                                    <p>${card.frontFace}</p>
-                                    <p>${card.backFace}</p>
-                                </div>
+                               <div class="card-inner"
+                                    <div class="front-face">
+                                        <p>${card.frontFace}</p>
+                                    </div>
+                                    <div class="back-face">
+                                         <p>${card.backFace}</p>
+                                    </div>
+                                </div> 
                         </div>
                         `)
-                        console.log(card)
                     })// end of for each
                 })
 }// end of getCardList function
