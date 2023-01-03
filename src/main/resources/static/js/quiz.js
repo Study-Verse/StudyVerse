34e5367cc425536fe4a822cbc98eaea1ea737edd
Fernando Lopez <flopez00032@gmail.com>
2023-01-03 11:51:41 -0600

    added quiz structure in html self test

--- src/main/resources/static/js/quiz.js
+++ src/main/resources/static/js/quiz.js
@@ -1,5 +1,11 @@
 $(function (){
 
-
+fetch("https://opentdb.com/api.php?amount=10")
+    .then(response => response.json())
+    .then(data =>
+        {
+            console.log(data)
+        }
+    )
 
 })
