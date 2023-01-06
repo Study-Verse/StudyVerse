7feed15662a54f0c34b37a882c3490842e7725b4
Fernando Lopez <flopez00032@gmail.com>
2023-01-06 15:19:32 -0600

    added a tag to quiz and aded method to find quiz by tag in the quiz repository

--- src/main/java/com/studyverse/Repositories/QuizRepository.java
+++ src/main/java/com/studyverse/Repositories/QuizRepository.java
@@ -1,7 +1,10 @@
 package com.studyverse.Repositories;
 
 import com.studyverse.Models.Quiz;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface QuizRepository {
+public interface QuizRepository extends JpaRepository<Quiz, Long> {
     Quiz findQuizById (long id);
+
+    Quiz findByTag(String tag);
 }
