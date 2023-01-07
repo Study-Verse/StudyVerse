d9624af80a2cfcd9908efe68d5863e71929c6c10
Fernando Lopez <flopez00032@gmail.com>
2023-01-07 12:39:56 -0600

    linking my own api

--- src/main/resources/static/js/quiz.js
+++ src/main/resources/static/js/quiz.js
@@ -1,3 +1,9 @@
 $(function (){
+    console.log("hey")
+    fetch("/current-user-quiz-api/api")
+        .then(resp=> resp.json())
+        .then(data => {
+            console.log(data)
+        })
 
 })
