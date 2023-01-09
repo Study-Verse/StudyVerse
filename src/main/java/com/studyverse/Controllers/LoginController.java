f0f24ff7d8c25b8ae1269b32f2eabfef2d02c04a
Valeria Canizales <vcanizales93@gmail.com>
2023-01-09 11:40:14 -0600

    added studyverse logo to splashpage

--- src/main/java/com/studyverse/Controllers/LoginController.java
+++ src/main/java/com/studyverse/Controllers/LoginController.java
@@ -1,12 +1,18 @@
 package com.studyverse.Controllers;
 
+import com.studyverse.Repositories.UserRepository;
+import com.studyverse.Services.Utils;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LoginController {
+
     @GetMapping("/login")
     public String showLoginForm(){
         return "login";
     }
+
+
 }
