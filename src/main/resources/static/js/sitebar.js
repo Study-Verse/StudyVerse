e6894041176d51458f3ea4f83aff35465255f791
David Lara <david.a.lara05@gmail.com>
2023-01-09 11:42:45 -0600

    working on wiki

--- src/main/resources/static/js/sitebar.js
+++ src/main/resources/static/js/sitebar.js
@@ -5,11 +5,46 @@ $(document).ready(function() {
     $("#site-bar-logout-link").on("click", function() {
         $("#site-bar-logout-form").submit();
     });
+
+
+
+
+
+
+
 //  Search bar
 $("#site-bar-search-link").on("click", function (){
     let inputSearchText = $("#searchForCards").val();
-    window.location.replace(`/search/${inputSearchText}`)
-    console.log(inputSearchText)
-});
+    window.location.replace(`/search/${inputSearchText}`);
+    console.log(inputSearchText);
+
+        let url = "https://en.wikipedia.org/w/api.php?action=opensearch&search="+ inputSearchText +"&format=json&callback=?";
+        $.ajax({
+            url: url,
+            type: 'GET',
+            contentType: "application/json; charset=utf-8",
+            async: false,
+            dataType: "json",
+
+            success: function(data) {
+                console.log(data);
+                $(".outputWikiSearch").html();
+                for(let i=0;i<data[1].length;i++){
+                    $(".outputWikiSearch").prepend("<div><div class='well'><a href="+data[3][i]+"><h2>" + data[1][i]+ "</h2>" + "<p>" + data[2][i] + "</p></a></div></div>");
+                }
+            }
+        })
+            .done(function() {
+                console.log("success");
+            })
+            .fail(function() {
+                console.log("error");
+            })
+            .always(function() {
+                console.log("complete");
+            });
+
+
+    });
 
 });
